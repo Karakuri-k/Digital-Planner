06f924a1254a80bd8499a569ae2d89df106ad7ae
Karakuri-k <k.kurisakisagberg@gmail.com>
2025-02-05 13:12:14 +0100

    NOTATENE FUNKERRRR!!!!

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ const header = document.querySelector('.calendar h3')
 const dates = document.querySelector('.dates')
 const navs = document.querySelectorAll('#prev, #next')
 
+
+//const minListe = ['John', 'Doe', 'Jane', 'Doe']
+//localStorage.setItem('plan', JSON.stringify(minListe))
+//const liste = JSON.parse(localStorage.getItem('plan'))
+//console.log(liste )
+
+
+
 const months = [
-    "January", 
-    "February", 
-    "March", 
-    "April", 
-    "May", 
-    "June", 
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
     "July",
     "August",
     "September",
@@ -22,27 +30,27 @@ let month = date.getMonth()
 let year = date.getFullYear()
 
 function renderCalendar() {
-    
+
     const start = new Date(year, month, 1).getDay()
     const endDate = new Date(year, month + 1, 0).getDate()
     const end = new Date(year, month, endDate).getDay()
     const endDatePrev = new Date(year, month, 0).getDate()
-    
+
     let datesHtml = ""
-    for(let i = start; i > 0; i--) {
+    for (let i = start; i > 0; i--) {
         datesHtml += `<li class="inactive">${endDatePrev - i + 1}</li>`
     }
 
-    for(let i = 1; i <= endDate; i++) {
-        let className = 
+    for (let i = 1; i <= endDate; i++) {
+        let className =
             i === date.getDate() &&
-            month === new Date().getMonth() &&
-            year === new Date().getFullYear() 
-            ? ' class="today"' 
-            : ""
+                month === new Date().getMonth() &&
+                year === new Date().getFullYear()
+                ? ' class="today"'
+                : ""
         datesHtml += `<li${className}>${i}</li>`
     }
-    for(let i = end; i < 6; i++) {
+    for (let i = end; i < 6; i++) {
         datesHtml += `<li class="inactive">${i - end + 1}</li>`
     }
 
@@ -70,8 +78,20 @@ navs.forEach((nav) => {
         date = new Date(year, month, new Date().getDate())
         year = date.getFullYear()
         month = date.getMonth()
-        
+
         renderCalendar()
     })
 })
 renderCalendar()
+
+document.getElementById("save").addEventListener("click", function () {
+    let notes = document.getElementById('notes').value
+    localStorage.setItem('notes', notes)
+    console.log("funket")
+}, false)
+
+
+window.onload = function () {
+    document.getElementById('notes').value = localStorage.getItem('notes')
+    console.log("lastet inn")
+}
